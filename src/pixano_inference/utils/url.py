0e4339b6c3f4353b4c8ff0f3452481dbdb3163d3
Julien Denize <julien.denize@cea.fr>
2025-02-18 15:36:08 +0100

    Change URL validation

--- src/pixano_inference/utils/url.py
+++ src/pixano_inference/utils/url.py
@@ -9,17 +9,10 @@
 import re
 
 
-url_validation_regex = re.compile(  # from Django
-    r"^(?:http|ftp)s?://"  # http:// or https://
-    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
-    r"localhost|"  # localhost...
-    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
-    r"(?::\d+)?"  # optional port
-    r"(?:/?|[/?]\S+)$",
-    re.IGNORECASE,
-)
+# TODO: improve this regex pattern
+url_validation_regex = r"^(file://|https?://|s3://)[^\s]+$"
 
 
 def is_url(url: str) -> bool:
     """Check if a string is a valid URL."""
-    return url_validation_regex.match(url) is not None
+    return re.match(url_validation_regex, url) is not None
